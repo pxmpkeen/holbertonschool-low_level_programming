44a85330244a59f7e3f56e0b22449e7f52963b18
pxmpkeen <pxmpkeen@gmail.com>
2023-10-12 08:47:51 -0700

    101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int l = strlen(s);
+	int i;
+
+	for (i = 0; i < l; i++)
+	{
+		putchar(s[i]);
+	}
+	return (1);
+}
