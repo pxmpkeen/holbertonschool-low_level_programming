753101d755c713ce0c1d6a15091c2ce22c326b22
pxmpkeen <pxmpkeen@gmail.com>
2023-10-16 13:57:14 -0700

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,19 +12,19 @@ void jack_bauer(void)
 
 	for (i = 48; i <= 50; i++)
 	{
-		for (j = 48; j <= 51; j++)
+		for (j = 48; j <= 57; j++)
 		{
 			for (k = 48; k <= 53; k++)
 			{
 				for (l = 48; l <= 57; l++)
 				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(k);
-					_putchar(l);
-					if (!(i == 50 && j == 51 && k == 53 && l == 57))
+					if ((10 * i + j) <= 551 && (10 * k + l) <= 587)
 					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
 						_putchar('\n');
 					}
 				}
