8d2764445e93cb1a11b51593dd272f2d141d427a
pxmpkeen <pxmpkeen@gmail.com>
2023-10-16 08:27:04 -0700

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int l = strlen(s);
+	int i;
+
+	for (i = 0; i <= l; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
