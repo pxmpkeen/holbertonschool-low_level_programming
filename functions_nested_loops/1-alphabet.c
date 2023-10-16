424a5908bf5bb4de8653fde3434bc7762c98d709
pxmpkeen <pxmpkeen@gmail.com>
2023-10-16 08:40:54 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	print_alphabet(void);
+	print_alphabet();
 	_putchar('\n);
 	return (0);
 }
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -6,7 +6,7 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
-void print_alphabet(void)
+void print_alphabet()
 {	
 	int i;
 
