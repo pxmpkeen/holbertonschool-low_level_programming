0fe02e719794678ecf788b21b9068c7f284ebe7e
pxmpkeen <pxmpkeen@gmail.com>
2023-10-16 08:39:17 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet(void);
+	_putchar('\n);
+	return (0);
+}
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -5,3 +5,13 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void print_alphabet(void)
+{	
+	int i;
+
+	for (i = 97; i < 122; i++)
+	{
+		_putchar(i);
+	}
+}
