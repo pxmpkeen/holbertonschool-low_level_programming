bc220ef5f8e32e50819842cf6cc326d53d91e6e2
pxmpkeen <pxmpkeen@gmail.com>
2023-10-16 09:57:00 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The number to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n + 2 * n);
+	}
+}
