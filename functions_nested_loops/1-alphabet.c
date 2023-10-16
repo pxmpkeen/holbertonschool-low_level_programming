b000b69ef6e7e764265b8d53dec15e4ee52851f9
pxmpkeen <pxmpkeen@gmail.com>
2023-10-16 08:59:01 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_alphabet: prints the alphabet
+ * print_alphabet - prints the alphabet
  *
  * Return: Always 0 (Success)
  */
@@ -10,7 +10,7 @@ void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i < 122; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
