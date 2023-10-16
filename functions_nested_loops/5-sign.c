2f89dae1dc998204e6215a574a4e2e51a97367ee
pxmpkeen <pxmpkeen@gmail.com>
2023-10-16 09:46:22 -0700

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: The number to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar('\n');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar('\n');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	}
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 2-print_alphabet_x10.c prints 10 times the alphabet, in lowercase, followed by a new line.
 3-islower.c checks for lowercase character.
 4-isalpha.c checks for alphabetic character.
+5-sign.c prints the sign of a number.
