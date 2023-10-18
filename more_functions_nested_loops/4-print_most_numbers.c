2764a42be314f0158a7b17a1b671a406394847d1
pxmpkeen <pxmpkeen@gmail.com>
2023-10-18 09:07:11 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - checks
+ *
+ * Return: 1 if upper, otherwise 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + 48);
+		}
+	}
+	_putchar('\n');
+}
