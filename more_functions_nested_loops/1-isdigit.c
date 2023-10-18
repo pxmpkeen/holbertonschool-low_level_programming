ce91c172c944c95880cebaad627052c66b9d849c
pxmpkeen <pxmpkeen@gmail.com>
2023-10-18 08:56:09 -0700

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - checks
+ * @c: checker
+ *
+ * Return: 1 if upper, otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
