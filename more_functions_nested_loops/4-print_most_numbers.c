3dc8a1656c00d1013bc93d08ccb5c4ea1d2ec677
pxmpkeen <pxmpkeen@gmail.com>
2023-10-18 09:13:41 -0700

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,10 @@
 #include "main.h"
-#include "_putchar.c"
 /**
  * print_most_numbers - checks
  *
  * Return: 1 if upper, otherwise 0
  */
-int main(void)
-{
-	print_most_numbers();
-}
+
 void print_most_numbers(void)
 {
 	int i;
