75dbaac4db23cf129645db802162d154b4110a8a
pxmpkeen <pxmpkeen@gmail.com>
2023-10-18 09:04:57 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - checks
+ *
+ * Return: 1 if upper, otherwise 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
