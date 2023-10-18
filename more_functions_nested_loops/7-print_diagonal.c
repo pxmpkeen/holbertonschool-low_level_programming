870d1ca1f6fcf4bbbca99c487be418179d0fe389
pxmpkeen <pxmpkeen@gmail.com>
2023-10-18 09:51:08 -0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - checks
+ * @n: times
+ *
+ * Return: 1 if upper, otherwise 0
+ */
+void print_diagonal(int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		int i = 0;
+
+		while (i <= j)
+		{
+			_putchar(' ');
+			i++;
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
