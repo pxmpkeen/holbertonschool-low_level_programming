35b5ccfdbdb2fd6a1227164fb95fd19b3b628e69
pxmpkeen <pxmpkeen@gmail.com>
2023-10-23 09:36:46 -0700

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - string copy
+ * @dest: dest
+ * @src: src
+ *
+ * Return: return
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
