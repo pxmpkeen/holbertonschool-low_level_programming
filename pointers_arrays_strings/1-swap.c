e60ab6ed48835ae0a4201b293cce4fedfa95a4ec
pxmpkeen <pxmpkeen@gmail.com>
2023-10-23 07:05:41 -0700

    2-strlen.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - resets to 98
+ * @a: num
+ * @b: num
+ *
+ * Return: just a return
+ */
+void swap_int(int *a, int *b)
+{
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -1,1 +1,3 @@
 0-reset_to_98.c takes a pointer to an int as parameter and updates the value it points to to 98.
+1-swap.c swaps the values of two integers.
+2-strlen.c returns the length of a string.
