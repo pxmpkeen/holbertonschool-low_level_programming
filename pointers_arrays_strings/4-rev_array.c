d3b1176ac6c5dee1e6bcd9431ec1694e8d1bafd5
pxmpkeen <pxmpkeen@gmail.com>
2023-10-24 09:08:16 -0700

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - reverses array
+ * @a: array
+ * @n: count
+ *
+ * Return: void...
+ */
+void reverse_array(int *a, int n)
+{
+        int i = 0, tmp;
+
+        while (i < n / 2)
+        {
+                tmp = *(a + i);
+                *(a + i) = *(a + n - i - 1);
+                *(a + n -  i - 1) = tmp;
+		i++;
+        }
+}
