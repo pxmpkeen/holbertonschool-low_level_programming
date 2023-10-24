b4e487e992123d3dce375e09fe64b49ee21c3203
pxmpkeen <pxmpkeen@gmail.com>
2023-10-24 09:09:27 -0700

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-        int i = 0, tmp;
+	int i = 0, tmp;
 
-        while (i < n / 2)
-        {
-                tmp = *(a + i);
-                *(a + i) = *(a + n - i - 1);
-                *(a + n -  i - 1) = tmp;
+	while (i < n / 2)
+	{
+		tmp = *(a + i);
+		*(a + i) = *(a + n - i - 1);
+		*(a + n -  i - 1) = tmp;
 		i++;
-        }
+	}
 }
