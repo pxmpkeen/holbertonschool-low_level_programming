ce48137ab0dabe16ae0023fb65301658d40019cd
pxmpkeen <pxmpkeen@gmail.com>
2023-10-24 04:18:21 -0700

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,38 @@
 #include "main.h"
 /**
  * _atoi - converts str to int
- *  @s: str
+ * @s: str
  *
  * Return: return
  */
-int main(void)
-{
-    int nb;
-
-    nb = _atoi("98");
-    printf("%d\n", nb);
-    nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-    printf("%d\n", nb);
-    nb = _atoi("214748364");
-    printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    printf("%d\n", nb);
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);
-    return (0);
-}
 int _atoi(char *s)
 {
+	int i = 0, m = 0;
+	unsigned int n = 0;
+
+	while (*(s + i) != 0)
+	{
+		if (*(s + i) == 45)
+		{
+			m++;
+		}
+
+		if (*(s + i) >= 48 && *(s + i) <= 57)
+		{
+			n = n * 10 + (*(s + i) - 48);
+
+			if (*(s + i + 1) < 48 || *(s + i + 1) > 57)
+			{
+				break;
+			}
+		}
+
+		i++;
+	}
+	if (m % 2 == 1)
+	{
+		n = -n;
+	}
+
+	return (n);
 }
