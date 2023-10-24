f872b96c8559d00eadde53b7fcb822d6d989a4f7
pxmpkeen <pxmpkeen@gmail.com>
2023-10-24 06:21:36 -0700

    0-strncat.c

---
+++ pointers_arrays_strings/0-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenates strings
+ * @dest: dstination
+ * @src: source
+ * @n: counter
+ *
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != 0)
+	{
+		i++;
+	}
+	while (j != n)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+
+	*(dest + i) = 0;
+
+	return (dest);
+}
