c3a2e19be2b29481de5ad96e66f10ec4f691b1a3
pxmpkeen <pxmpkeen@gmail.com>
2023-10-25 07:26:35 -0700

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * leet - leets
+ * @s: s
+ *
+ * Return: char
+ */
+char *leet(char *s)
+{
+	int i = 0, j = 0;
+	char *letter;
+	char num[] = "43071";
+	char uc[] = "AEOTL";
+	char lc[] = "aeotl";
+
+	while (*(s + i))
+	{
+		letter = (s + i);
+		j = 0;
+		while (j < 5)
+		{
+			if (*letter == *(uc + j) || *letter == *(lc + j))
+			{
+				*letter = *(num + j);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (s);
+}
