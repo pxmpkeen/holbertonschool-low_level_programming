fb582903df4555470021646bd5158249f87535e7
pxmpkeen <pxmpkeen@gmail.com>
2023-10-26 06:52:13 -0700

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - fills memory
+ * @s: area
+ * @b: byte
+ * @n: counter
+ *
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+
+	return (s);
+}
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
+
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);
+    return (0);
+}
---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * print_number - prints number
+ * @n: int
+ *
+ * Return: bruh
+ */
+void print_number(int n)
+{
+	unsigned int k;
+	while (n / 10)
+	{
+	}
+}
