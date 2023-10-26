48172a515a7d7f96d5d1acae8cfe54d216ca7f26
pxmpkeen <pxmpkeen@gmail.com>
2023-10-26 09:56:41 -0700

    3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - does smth strange
+ * @s: string
+ * @accept: string2
+ *
+ * Return: un
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j, k = 0, n = 0;
+
+	while (*(s + i))
+	{
+		j = 0;
+		k = 0;
+		while (*(accept + j))
+		{
+			if (*(s + i) != *(accept + j))
+			{
+				k++;
+				n = i;
+			}
+			j++;
+		}
+		if (k == j)
+			return (n);
+		i++;
+	}
+}
