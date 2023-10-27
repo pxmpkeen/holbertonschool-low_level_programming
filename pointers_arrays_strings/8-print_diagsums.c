0fa345eeffc4068eb1fa187c8351c71e7812a9d1
pxmpkeen <pxmpkeen@gmail.com>
2023-10-27 08:12:29 -0700

    8-print_diagsums.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagsums - prints sums of main diagonals
+ * @a: a
+ * @size: size of matrix in one dimension
+ *
+ * Return: w/o
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0, l = 0, r = 0;
+
+	while (i < size)
+	{
+		l += a[i * size + i];
+		r += a[i * size + (size - i - 1)];
+		i++;
+	}
+	printf("%d, %d\n", l, r);
+}
