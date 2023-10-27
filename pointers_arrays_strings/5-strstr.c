7b135d238300f62e8bbcbe9b2418e6d98e5578d1
pxmpkeen <pxmpkeen@gmail.com>
2023-10-27 06:31:51 -0700

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr - func
+ * @haystack: char
+ * @needle: char
+ *
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+	int i = 0;
+
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n)
+		{
+			h++;
+			n++;
+		}
+		if (!(*n))
+			return (haystack);
+		haystack++;
+	}
+
+	return (NULL);
+}
