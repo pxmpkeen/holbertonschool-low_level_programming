f54e8ce07adb7847a4a4392c28fcc7b7ab687794
pxmpkeen <pxmpkeen@gmail.com>
2023-10-29 09:56:34 -0700

    101-print_number.c

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,15 +1,29 @@
 #include "main.h"
-#include "_putchar.c"
 /**
  * print_number - prints number
  * @n: int
  *
  * Return: bruh
  */
+void print(int);
+
 void print_number(int n)
 {
+	print(n);
+}
+void print(int n)
+{
 	unsigned int k;
-	while (n / 10)
-	{
+
+	if (n < 0) {
+		_putchar(45);
+		k = -n;
 	}
+	else
+		k = n;
+
+	if (k / 10)
+		print(k / 10);
+
+	_putchar(k % 10 + '0');
 }
