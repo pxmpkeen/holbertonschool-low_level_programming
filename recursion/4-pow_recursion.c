74f820e6c6cfafde281849c5973a047c440915b7
pxmpkeen <pxmpkeen@gmail.com>
2023-10-30 05:47:25 -0700

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,11 +11,10 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	if (y == 0)
-		return (1);
-	if (y != 1)
+	else if (y != 1)
 	{
-		y--;
-		return (_pow_recursion(x, y) * x);
+		return (_pow_recursion(x, y - 1) * x);
 	}
+	else if (y == 0)
+		return (1);
 }
