5e26a94eaa64c07f19aa288d124e6cb9fa9610a7
pxmpkeen <pxmpkeen@gmail.com>
2023-10-30 05:32:56 -0700

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - calculates factorial
+ * @n: number
+ *
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (i < 0)
+		return (-1);
+	else if (i <= 1)
+		return (1);
+	return (i * factorial(i - 1));
+}
