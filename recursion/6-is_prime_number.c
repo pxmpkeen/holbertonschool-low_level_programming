0f900d7e4f7c5925a543bdbf3d9e9db5be896af1
pxmpkeen <pxmpkeen@gmail.com>
2023-10-30 06:57:35 -0700

    5-sqrt_recursion.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _helper - helper
+ * @n: n
+ * Return: int
+ */
+int _helper(int x, int y)
+{
+	if (x * x == y)
+		return (x);
+	else if (x * x > y)
+		return (-1);
+	return (_helper(x + 1, y));
+}
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @x :checker
+ * @y: checker
+ *
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+
+	if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+	return (_helper(1, n));
+}
