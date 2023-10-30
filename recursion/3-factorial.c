a472aab152dcd5bf9a738dcc026141f60bb6c36f
pxmpkeen <pxmpkeen@gmail.com>
2023-10-30 05:33:42 -0700

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -7,9 +7,9 @@
  */
 int factorial(int n)
 {
-	if (i < 0)
+	if (n < 0)
 		return (-1);
-	else if (i <= 1)
+	else if (n <= 1)
 		return (1);
-	return (i * factorial(i - 1));
+	return (n * factorial(n - 1));
 }
