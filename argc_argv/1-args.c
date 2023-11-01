1d25d9257765577b9502f6be26531953bccbe8cc
pxmpkeen <pxmpkeen@gmail.com>
2023-11-01 07:27:50 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
