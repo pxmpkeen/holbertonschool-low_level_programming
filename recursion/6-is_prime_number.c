ecccd38ca589ee7ea41bc2c7a3d5a2bc1c995178
pxmpkeen <pxmpkeen@gmail.com>
2023-11-01 04:37:15 -0700

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,30 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _helper - helper
- * @n: n
+ * @x: x
+ * @y: y
  * Return: int
  */
 int _helper(int x, int y)
 {
-	if (x * x == y)
-		return (x);
-	else if (x * x > y)
-		return (-1);
-	return (_helper(x + 1, y));
+	if (x > y / 2)
+		return (1);
+	else if (!(y % x))
+		return (0);
+	else
+		return (_helper(x + 1, y));
 }
 /**
- * _sqrt_recursion - natural square root of a number
- * @x :checker
- * @y: checker
+ * is_prime_number - checks whether number is prime
+ * @n: checker
  *
  * Return: int
  */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
-
-	if (n == 0)
+	if (n < 0 || n == 1)
 		return (0);
-	else if (n < 0)
-		return (-1);
-	return (_helper(1, n));
+	return (_helper(2, n));
 }
