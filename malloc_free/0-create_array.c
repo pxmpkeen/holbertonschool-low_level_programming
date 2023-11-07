0fd96e0800c80a839960abf35a643833d54560a0
pxmpkeen <pxmpkeen@gmail.com>
2023-11-07 10:55:45 -0800

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars
+ * @size: size of malloc
+ * @c: char
+ *
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	int i = 0;
+
+	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		*(ptr + i) = c;
+		i++;
+	}
+
+	return (ptr);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+0-create_array.c - creates an array of chars, and initializes it with a specific char.
