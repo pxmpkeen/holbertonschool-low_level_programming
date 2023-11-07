d99c99e6098134f568d3c7ef180e3b6aa02e305a
pxmpkeen <pxmpkeen@gmail.com>
2023-11-07 11:45:35 -0800

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _strdup -  returns a pointer to a newly allocated space in memory
+ * @str: char
+ *
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+	char *s;
+
+	while (*(str + i))
+		i++;
+
+	if (i == 0)
+		return (NULL);
+
+	s = malloc(sizeof(char) * i);
+
+	while (j <= i)
+	{
+		s[j] = *(str + j);
+		j++;
+	}
+
+	return (s);
+}
