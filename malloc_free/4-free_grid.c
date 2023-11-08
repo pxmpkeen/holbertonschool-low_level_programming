eaa9989813653036848bf7c6a16440dbd8864eb3
pxmpkeen <pxmpkeen@gmail.com>
2023-11-08 08:03:21 -0800

    4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -13,7 +13,10 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = height; i >= 0; i--)
-		free(grid[i]);
-	free(grid);
+	if (grid != NULL)
+	{
+		for (i = height; i >= 0; i--)
+			free(grid[i]);
+		free(grid);
+	}
 }
