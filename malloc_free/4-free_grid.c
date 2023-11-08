8bafc7704421a5b7e967eb64e86555df063b3fc3
pxmpkeen <pxmpkeen@gmail.com>
2023-11-08 07:58:57 -0800

    4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees mallocs
+ * @grid: matrix
+ * @height: height
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height);
+{
+	int i;
+
+	for (i = height; i >= 0; i--)
+		free(grid[i]);
+	free(grid);
+}
