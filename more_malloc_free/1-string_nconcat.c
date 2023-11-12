05e58fbac37ef8e558aedd4303abda77f001cab8
pxmpkeen <pxmpkeen@gmail.com>
2023-11-12 06:37:41 -0800

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * string_nconcat - concatenates strings
+ * @s1: first
+ * @s2: second
+ * @n: int
+ *
+ * Return: char
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i = 0, j = 0;
+
+
+	if (s1 == NULL)
+		i = 1;
+	else
+	{
+		while (*(s1 + i))
+			i++;
+	}
+
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		while (*(s2 + j))
+			j++;
+	}
+
+	if (n >= j)
+		n = j;
+
+	str = malloc(sizeof(char) * (i + n + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+
+	while (*(s1 + i))
+	{
+		*(str + i) = *(s1 + i);
+		i++;
+	}
+	while (j < n)
+	{
+		*(str + i) = *(s2 + j);
+		j++;
+		i++;
+	}
+
+	str[i] = 0;
+
+	return (str);
+}
