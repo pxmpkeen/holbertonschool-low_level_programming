62444e439b2407c73ea217924cd66ac4c319c05e
pxmpkeen <pxmpkeen@gmail.com>
2023-11-13 04:05:43 -0800

    task

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stddef.h>
-#include <stdio.h>
+#include <string.h>
 /**
  * string_nconcat - concatenates strings
  * @s1: first
@@ -13,42 +13,31 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i = 0, j = 0;
+	int cl = (s2 != NULL && strlen(s2) <= n);
+	int cg = (s2 != NULL && strlen(s2) > n);
+	unsigned int i, l1 = (s1 == NULL) ? 0 : strlen(s1);
+	unsigned int l2 = 0 * (s2 == NULL) + cl * strlen(s2) + cg * n;
 
+	str = malloc(sizeof(char) * (l1 + l2 + 1));
 
-	if (s1 == NULL)
-		i = 0;
-	else
-	{
-		while (*(s1 + i))
-			i++;
-	}
-	if (s2 == NULL)
-		j = 0;
-	else
-	{
-		while (*(s2 + j))
-			j++;
-	}
-	if (n >= j)
-		n = j;
-	str = malloc(sizeof(char) * (i + n + 1));
 	if (str == NULL)
 		return (NULL);
+
+	while (i < l1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+
 	i = 0;
-	j = 0;
-	while (*(s1 + i))
+
+	while (i < l2)
 	{
-		*(str + i) = *(s1 + i);
+		str[i + l1] = s2[i];
 		i++;
 	}
-	while (j < n)
-	{
-		*(str + i) = *(s2 + j);
-		j++;
-		i++;
-	}
-	str[i] = 0;
+
+	str[l1 + l2] = 0; 
 
 	return (str);
 }
