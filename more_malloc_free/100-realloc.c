71e662cf06830b141de306a4fdef9eaf0b1db98e
pxmpkeen <pxmpkeen@gmail.com>
2023-11-14 05:15:01 -0800

    task

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _realloc - realloc
+ * @ptr: ptr 
+ * @old_size: old_size
+ * @new_size: new_size
+ *
+ * Return: void *
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	int i = 0;
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+
+	while (i < ((old_size <= new_size) ? old_size : new_size))
+	{
+		*(p + i) = *(char *)(ptr + i);
+		i++;
+	}
+	free(ptr);
+
+	return (p);
+}
