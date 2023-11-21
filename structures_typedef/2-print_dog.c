29a8f577331e05b4ce34feb8b88c12814429d9a7
pxmpkeen <pxmpkeen@gmail.com>
2023-11-21 04:05:58 -0800

    task

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints dog struct
+ * @d: struct dog
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	(*d).name == NULL ? printf("Name: (nil)\n") : printf("Name: %s\n", (*d).name);
+	(*d).age == NULL ? printf("Age: (nil)\n") : printf("Age: %s\n", (*d).age);
+	(*d).owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %s\n", (*d).owner);
+}
