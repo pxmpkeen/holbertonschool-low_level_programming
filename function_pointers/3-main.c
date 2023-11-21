201d32aa78a5f5d3a48e24b126cb0d44ff4633a7
pxmpkeen <pxmpkeen@gmail.com>
2023-11-21 07:45:29 -0800

    task

---
+++ function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdib.h>
+/**
+ * main - entry point
+ * @argc - argc
+ * @argv - argv
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int m, n;
+	char *op = argv[2];
+	
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	m = atoi(argv[1]), n = atoi(argv[3]);
+	if ((*op != '*' && *op != '/' && *op != '+' && *op != '-' && *op != '%')
+			|| strlen(op) > 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' || *op == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(m, n));
+	return (0);
+}
