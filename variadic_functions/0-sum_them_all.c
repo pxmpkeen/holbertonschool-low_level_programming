f0ee02f52d5c5fe4bf28ee21042cd7e6c1318740
pxmpkeen <pxmpkeen@gmail.com>
2023-11-22 05:54:52 -0800

    0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - sums all
+ * @n: count
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int sum = 0, i;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+
+	return (sum);
+}
