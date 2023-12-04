482068f2b8b9f91679883ecf170c7df60c4e0261
pxmpkeen <pxmpkeen@gmail.com>
2023-12-04 04:39:17 -0800

    2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_node - insertion
+ * @head: node in linked list
+ * @str: string in node
+ * Return: size of nodes in linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node = malloc(sizeof(list_t));
+	if (!node)
+		return (NULL);
+	(*node).str = strdup(str);
+	(*node).len = strlen(str);
+	(*node).next = *head;
+	*head = node;
+	return (*head);
+}
