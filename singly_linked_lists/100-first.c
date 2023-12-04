9b02f50a0519c88569940afaaa4c57075d0ea96d
pxmpkeen <pxmpkeen@gmail.com>
2023-12-04 06:50:16 -0800

    100-first.c

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
+void func (void) __attribute__ ((constructor));
 /**
  * func - before main
  */
-void func (void) __attribute__ ((constructor))
+void func (void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
