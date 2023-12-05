f96d76867c8067ce377a5af6f0d2e2d952a3808c
pxmpkeen <pxmpkeen@gmail.com>
2023-12-05 05:10:03 -0800

    6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * sum_dlistint -  returns the sum of all the data (n)
+ * @head: address of node
+ * Return: the sum of all the data (n)
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int sum = 0;
+
+	if (!temp)
+		return (0);
+	while (temp)
+	{
+		sum += (*temp).n;
+		temp = (*temp).next;
+	}
+	return (sum);
+}
