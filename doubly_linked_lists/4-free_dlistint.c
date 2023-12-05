abd80530d99ab29dfb72ca9bb12ad25476af4ec7
pxmpkeen <pxmpkeen@gmail.com>
2023-12-05 04:17:27 -0800

    4-free_dlistint.c

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dlistint - frees everything
+ * @head: pointer to address of node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = (*head).next;
+		free((*temp).str);
+		free(temp);
+	}
+}
