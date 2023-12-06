8c17edf26b74a7bea1c6cb014da0c16cab1f3de4
pxmpkeen <pxmpkeen@gmail.com>
2023-12-06 04:29:45 -0800

    8-delete_dnodeint.c

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * delete_dnodeint_at_index - deletes a new node at a given position.
+ * @head: head
+ * @index: index of the list
+ * Return: node
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int temp_index = 0;
+	dlistint_t *temp = *head;
+
+	if (!head || !temp)
+		return (-1);
+	if (!index)
+	{
+		if ((*temp).next)
+			(*(**head).next).prev = NULL;
+		*head = (**head).next;
+		return (1);
+	}
+	while (temp_index < index)
+	{
+		if (!(*temp).next)
+			return (-1);
+		temp = (*temp).next;
+		temp_index++;
+	}
+	(*(*temp).prev).next = (*temp).next;
+	if ((*temp).next)
+		(*(*temp).next).prev = (*temp).prev;
+	return (1);
+}
+/*NIGAR <3*/
