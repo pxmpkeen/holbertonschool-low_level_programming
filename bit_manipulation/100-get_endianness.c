988ef16563cd423b576b6e5063889329d09cff70
pxmpkeen <pxmpkeen@gmail.com>
2023-12-08 07:16:02 -0800

    100-get_endianness.c

---
+++ bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_endianness - determines endiannes
+ * Return: 0 if BE, 1 if LE
+ */
+int get_endianness(void)
+{
+	short lab_rat_duo = 1; 
+	char *cell = (char *)&lab_rat_duo;
+
+	return (*cell);
+}
