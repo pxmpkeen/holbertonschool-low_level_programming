98ffacb41f00a8f735f1c9dc86a2fd11c3ade7b7
pxmpkeen <pxmpkeen@gmail.com>
2023-12-08 03:46:29 -0800

    2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * get_bit - gets bit
+ * @n: number
+ * @index: index
+ * Return: value of bit
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	n >>= index;
-	return ((!!n) * (n & 1) + (-1) * (!n));
+	return (n & 1);
 }
