494ffba5bdd053259bb81d083d54d3b267969acc
pxmpkeen <pxmpkeen@gmail.com>
2023-12-08 05:16:54 -0800

    4-clear_bit.c

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * clear_bit - clears bit
+ * @n: number
+ * @index: index
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int k = ((int)index < (int)sizeof(unsigned long int) * 8);
+
+	if (k)
+		*n &= ~(1 << index);
+	return (1 * k + (-1) * (!k));	
+}
