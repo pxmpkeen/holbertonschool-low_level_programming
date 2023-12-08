3b99799b1e2aa54676161a12503087e16fd7c888
pxmpkeen <pxmpkeen@gmail.com>
2023-12-08 05:06:31 -0800

    3-set_bit.c

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	int k = ((int)index < (int)sizeof(unsigned long int) * 8) && (*n >> index);
-
-	*n = *n | (1 << index);
+	if (k)
+		*n = *n | (1 << index);
 	return (1 * k + (-1) * (!k));
 }
