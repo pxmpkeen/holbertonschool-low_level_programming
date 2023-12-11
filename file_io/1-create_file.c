860d95abf7cf8cad07b0602ce9a2c7218deb34af
pxmpkeen <pxmpkeen@gmail.com>
2023-12-11 05:58:48 -0800

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,22 +8,32 @@
 /**
  * create_file - creates file
  * @filename: filename
- * @text_context: context
+ * @text_content: context
  * Return: int
  */
 int create_file(const char *filename, char *text_content)
 {
-	char *d = "";
-	int fd, w;
+	int fd, w, count;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
-	fd = open(filename, O_RDONLY | O_CREAT, 600);
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
 
-	w = !text_content ? write(fd, d, 1) :
-		write(fd, text_content, strlen(text_content));
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		count = strlen(text_content);
+	else
+	{
+		close(fd);
+		return (-1);
+	}
+
+	w = write(fd, text_counter, count);
+	if (output != -1 || output != count)
+		return (-1);
 
 	close(fd);
-	return (w);
+	return (1);
 }
