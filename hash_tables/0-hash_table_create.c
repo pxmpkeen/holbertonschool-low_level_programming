574f00abb912b61deaf45719d9d80910b8251f87
pxmpkeen <pxmpkeen@gmail.com>
2023-12-13 06:16:48 -0800

    0-hash_table_create.c

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (!table)
+		return (NULL);
+	(*table).size = size;
+	return (table);
+}
