257aba83dcd6738e0979c26cccc51d8ba7bf4937
pxmpkeen <pxmpkeen@gmail.com>
2023-12-16 10:29:12 -0800

    5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,13 +1,14 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 /**
  * hash_table_print - prints item of hash table
  * @ht: hash_table_t
  */
 void hasoh_table_print(const hash_table_t *ht)
 {
-	unsigned long int index = 0;
+	unsigned long int i = 0;
 	hash_node_t *temp;
 
 	if (!ht)
