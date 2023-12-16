db34044a37e357b9b8df6472704d1acac0ed9a70
pxmpkeen <pxmpkeen@gmail.com>
2023-12-16 05:54:52 -0800

    4-hash_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * hash_table_get - gets item of hash table
+ * @ht: hash_table_t
+ * @key: key
+ * Return: char *
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (!key || !ht || !(*key))
+		return (NULL);
+	index = key_index(key, (*ht).size);
+	temp = (*ht).array[index];
+	while (temp)
+	{
+		if (!strcmp((*temp).key, key))
+			break;
+		temp = (*temp).next;
+	}
+	if (temp)
+		return ((*temp).value);
+	else
+		return (NULL);
+}
