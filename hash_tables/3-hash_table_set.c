4cb8d692d75e6e4be213770d288951c286f2c543
pxmpkeen <pxmpkeen@gmail.com>
2023-12-16 04:13:36 -0800

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || !(*key) || !value)
 		return (0);
 
-	index = key_index(const unsigned char *key, (*ht).size);
+	index = key_index(key, (*ht).size);
 	temp = (*ht).array[index];
 
 	while ((*temp).next)
