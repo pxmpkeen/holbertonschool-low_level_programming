62c5e67c4a93e0b876bd47dd7f1a234139817f0f
pxmpkeen <pxmpkeen@gmail.com>
2023-12-16 12:13:13 -0800

    6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,35 +1,42 @@
-#include "hash_tables.h"
 #include <stdlib.h>
+#include "hash_tables.h"
 #include <string.h>
-#include <stdio.h>
 /**
- * hash_table_delete - deletes item of hash table
- * @ht: hash_table_t
- */
+  * hash_table_delete - deletes a hash table.
+  * @ht: hash_table
+  * Return:  actual number of letters it could read and print
+  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0;
-	hash_node_t *temp, *node;
-/**
- * temp - present node
- * node = (*temp).next
- */
-	if (!ht)
-		return;
+	unsigned long int len, i = 0;
+	hash_node_t *temp1, *temp2;
 
-	while (i < (*ht).size)
+	if (!ht)
 	{
-		temp = (*ht).array[i];
-		while (temp)
-		{
-			node = temp;
-			temp = (*temp).next;
-			free((*temp).key);
-			free((*temp).value);
-			free(node);
-		}
-		i++;
+		return;
 	}
-	free((*ht).array);
+	len = ht->size;
+	while (i < len)
+	{
+		if (ht->array[i])
+		{
+			temp1 = ht->array[i];
+			temp2 = ht->array[i]->next;
+			while (1)
+			{
+				free(temp1->key);
+				free(temp1->value);
+				free(temp1);
+				if (!temp2)
+					break;
+				temp1 = temp2;
+				temp2 = temp2->next;
+			}
+			i++;
+		}
+		else
+			i++;
+	}
+	free(ht->array);
 	free(ht);
 }
