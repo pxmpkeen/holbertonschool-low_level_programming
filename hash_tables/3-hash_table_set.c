2926de48d3f0b9958a4907537bb61e590fff9495
pxmpkeen <pxmpkeen@gmail.com>
2023-12-16 10:14:25 -0800

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -21,8 +21,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	node = malloc(sizeof(hash_node_t));
 
-	if (!temp)
-		malloc(sizeof(hash_node_t *));
 	if (!node)
 		return (0);
 
