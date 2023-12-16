33413db8663465b68579c69f47210ecdd85a20a7
pxmpkeen <pxmpkeen@gmail.com>
2023-12-16 05:33:25 -0800

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -32,6 +32,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!(*node).key || !(*node).value)
 		return (0);
 
+	if (temp)
+	{
+		if (!strcmp((*temp).key, key))
+		{
+			(*node).next = NULL;
+			(*ht).array[index] = node;
+			return (1);
+		}
+	}
 	(*node).next = temp;
 	(*ht).array[index] = node;
 
