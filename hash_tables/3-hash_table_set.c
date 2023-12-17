380e2aa40d294d053df34f30849cddf8ce4d2dab
pxmpkeen <pxmpkeen@gmail.com>
2023-12-17 04:13:12 -0800

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,15 +12,15 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node, *temp;
+	hash_node_t *node, *temp, *buffer;
 
 	if (!ht || !key || !(*key) || !value)
 		return (0);
 
 	index = key_index((unsigned char *)key, (*ht).size);
 	temp = (*ht).array[index];
-
 	node = malloc(sizeof(hash_node_t));
+
 	if (!node)
 		return (0);
 
@@ -33,40 +33,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	if (!temp)
+	(*node).next = temp;
+	(*ht).array[index] = node;
+	temp = node;
+
+	while ((*temp).next)
 	{
-		(*node).next = NULL;
-		(*ht).array[index] = node;
-		return (1);
-	}
-	else if (!(*temp).next)
-	{
-		(*node).next = NULL;
-		if (!strcmp((*temp).key, key))
-			free(temp), (*ht).array[index] = node;
-		else
-			(*temp).next = node;
-		return (1);
-	}
-	while (temp)
-	{
-		if ((*temp).next)
+		if (!strcmp((*(*temp).next).key, key))
 		{
-			if (!strcmp((*(*temp).next).key, key))
-			{
-				node = (*(*temp).next).next;
-				free((*temp).next);
-				(*temp).next = node;
-				temp = (*ht).array[index];
-				(*node).next = temp;
-				node = (*ht).array[index];
-				return (1);
-			}
+			buffer = (*temp).next;
+			(*temp).next = (*buffer).next;
+			free((*buffer).value), free((*buffer).key), free(buffer);
 		}
 		temp = (*temp).next;
 	}
-
-	(*node).next = NULL;
-	(*temp).next = node;
 	return (1);
 }
