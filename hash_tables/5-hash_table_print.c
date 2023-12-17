eeab34a78e0623bab850ede7a8a7863cb859f652
pxmpkeen <pxmpkeen@gmail.com>
2023-12-17 05:26:06 -0800

    5-hash_table_print.c

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdio.h>
+/**
+ * hash_table_print - prints hash table
+ * @ht: hash table itself
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+
+	if (!ht)
+		return;
+
+	printf("{");
+
+	while (i < (*ht).size)
+	{
+		temp = (*ht).array[i];
+		while (temp)
+		{
+		printf("'%s': '%s'", (*temp).key, (*temp).value);
+			if ((*temp).next)
+				printf(", ");
+			temp = (*temp).next;
+		}
+		if ((*ht).array[i + 1])
+			printf(", ");
+		i++;
+	}
+
+	printf("}\n");
+}
