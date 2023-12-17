95df1f3649b5a5dc771a1b529684718efc6d607b
pxmpkeen <pxmpkeen@gmail.com>
2023-12-17 05:06:06 -0800

    4-hash_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_get - returns value of given key
+ * @ht: hash table
+ * @key: key to find value
+ * Return: value of a node with key equals key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (!ht || !key || !*key)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, (*ht).size);
+	temp = (*ht).array[index];
+
+	while (temp)
+	{
+		if (strcmp((*temp).key, key))
+			return ((*temp).value);
+		temp = (*temp).next;
+	}
+	return (NULL);
+}
